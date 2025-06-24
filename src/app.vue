c8dac4c03201f299f3ef2bb3a1f74bac89d22f99
xdsun777 <charmgo@qq.com>
2025-06-24 23:21:55 +0800

    增加对wasm模块的错误处理

--- src/app.vue
+++ src/app.vue
@@ -2,20 +2,23 @@
     <RouterView />
 </template>
 <script>
-import rs_wasm from '@wasm-pkg/rs_wasm';
+
 // 调用wasm
-rs_wasm()
-    .then((wasm) => {
-        // console.log(wasm)
-    })
-    .catch(console.error);
+const rs_wasm = __WASM__ ? import('@wasm/rs_wasm') : false
+console.log('rs_wasm', rs_wasm);
+rs_wasm.then((wasm) => {
+    wasm.default();
+}).catch((e) => {
+    console.error('wasm error', e);
+});
+
+
 
 // 调用tauri 后端api,直接获取结果
 import tauriInvoke from '@utils/tauri.invoke.js';
 
 // 调用浏览器脚本
 import getSystemType from '@utils/system.js';
-import { RouterView } from 'vue-router';
 console.log(getSystemType());
 
 export default {};
